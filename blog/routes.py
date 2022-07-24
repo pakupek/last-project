d51391c2a6a198383987f42e20c0fd080cb68c5f
Adam Akimowicz <adam.akimowicz13@onet.eu>
2022-07-24 15:58:11 +0200

    fixed_issue

--- blog/routes.py
+++ blog/routes.py
@@ -1,10 +1,41 @@
-from flask import render_template
+from flask import render_template, request
 from blog import app
-from blog.models import Entry
-
+from blog.forms import EntryForm
+from blog.models import Entry, db
 
 #Route leading into homepage
 @app.route('/')
 def homepage():
-    all_posts = Entry.query.all()
-    return render_template('homepage.html', all_posts_=all_posts)
+    posts = Entry.query.all()
+    return render_template('homepage.html', posts=posts)
+
+#Route for adding posts
+@app.route('/addpost', methods=["GET","POST"])
+def create_entry():
+    form = EntryForm()
+    errors = None
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            entry = Entry(
+                title=form.title.data,
+                body=form.body.data,
+                is_published=form.is_published.data
+            )
+            db.session.add(entry)
+            db.session.commit()
+        else:
+            errors=form.errors
+    return render_template('entry_form.html', form=form, errors=errors)
+
+@app.route('/editpost/<int:entry_id>', methods=["GET", "POST"])
+def edit_post(entry_id):
+    entry = Entry.query.filter_by(id=entry_id).first_or_404()
+    form = EntryForm(obj=entry)
+    errors = None
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            form.populate_obj(entry)
+            db.session.commit()
+        else:
+            errors = form.errors
+    return render_template("entry_form.html", form=form, errors=errors)
